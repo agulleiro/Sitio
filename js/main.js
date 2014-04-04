44aec855a977409bc3eef7fb45aa8c6c15786bff
lucasmcarp <lucasm.alderete@gmail.com>
2014-04-04 03:44:44 -0300

    Se agrego scrollto a la navegacion de footer.

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(document).on('ready', init);
 
 function init()
 {
-	alert('pagina cargada');
+
 	$('#navegacionPrincipal').localScroll();//carga el localScroll, efecto copado para la navegacion
 	$('.navegacionFooter').localScroll();  
 }
