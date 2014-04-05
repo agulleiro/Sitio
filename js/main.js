79225df314d7cb8f45c352b51870dc927784c25b
wali <walter.dotto91@gmail.com>
2014-04-05 14:41:43 -0300

    Creando un logo circular y modificando para hacerlo encajar

--- js/main.js
+++ js/main.js
@@ -6,4 +6,8 @@ function init()
 
 	$('#navegacionPrincipal').localScroll();//carga el localScroll, efecto copado para la navegacion
 	$('.navegacionFooter').localScroll();  
+
+	alert($("h1").parent('#logo').text());
+	$("#logo").lettering();
+	
 }
