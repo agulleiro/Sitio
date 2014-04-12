81b7777f62b2a89d218b21c2903f6b97b100863b
Luqui <lucasm.alderete@gmail.com>
2014-04-11 23:46:00 -0300

    Contacto

--- enviarMail.php
+++ enviarMail.php
@@ -4,12 +4,19 @@ if(isset($_POST['asunto']) && isset($_POST['comentario']) && isset($_POST['nombr
 	!empty($_POST['asunto']) && !empty($_POST['comentario']) && !empty($_POST['nombre']) && !empty($_POST['email']))
 	{
 
-		/*falta codificar*/
-		echo "Correo enviado... <br>  - Falta codificar";
+		$mailLucas="lucasm.alderete@gmail.com";
+		$mailWalter="walter.dotto91@gmail.com";
+		$desde="From:"."SkySoft";
+		$asunto="[Comentario SkySoft] ".$_POST['asunto'];
+		$mensaje=" Nombre: ".$_POST['nombre']."\nEmail: ".$_POST['email']."\nMensaje: ".$_POST['comentario'];
+		mail($destino,$asunto,$mensaje,$mailLucas);
+		mail($destino,$asunto,$mensaje,$mailWalter);
+		header('Location: enviado.html'); 
 
 	}
 else
 	{
-		echo "Error: Campo/s vacio/s";
+		header('Location: noEnviado.html');
 	}
+
 ?>
