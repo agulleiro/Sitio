bcf09ec1cc022545d4d869f54be33927d14bcb43
Luqui <lucasm.alderete@gmail.com>
2014-04-17 19:13:02 -0300

    Limpieza de codigo, cambio de variables y de medidas de pxl a em

--- enviarMail.php
+++ enviarMail.php
@@ -4,13 +4,11 @@ if(isset($_POST['asunto']) && isset($_POST['comentario']) && isset($_POST['nombr
 	!empty($_POST['asunto']) && !empty($_POST['comentario']) && !empty($_POST['nombre']) && !empty($_POST['email']))
 	{
 
-		$mailLucas="lucas.alderete@skysoft.com.ar";
-		$mailWalter="walter.dotto@skysoft.com.ar";
+		$mailLucas="lucasm.alderete@gmail.com";
 		$desde="From:"."SkySoft";
 		$asunto="[Comentario SkySoft] ".$_POST['asunto'];
 		$mensaje=" Nombre: ".$_POST['nombre']."\nEmail: ".$_POST['email']."\nMensaje: ".$_POST['comentario'];
 		mail($destino,$asunto,$mensaje,$mailLucas);
-		mail($destino,$asunto,$mensaje,$mailWalter);
 		header('Location: enviado.html'); 
 
 	}
