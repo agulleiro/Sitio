f92ab616f7b23be5309e83716206595ff5b9ded6
Luqui <lucasm.alderete@gmail.com>
2014-04-17 23:24:48 -0300

    php Ok

--- enviarMail.php
+++ enviarMail.php
@@ -4,12 +4,14 @@ if(isset($_POST['asunto']) && isset($_POST['comentario']) && isset($_POST['nombr
 	!empty($_POST['asunto']) && !empty($_POST['comentario']) && !empty($_POST['nombre']) && !empty($_POST['email']))
 	{
 
-		$mailLucas="lucasm.alderete@gmail.com";
+		$mailLucas="lucas.alderete@skysoft.com.ar";
+		$mailWalter="walter.dotto@skysoft.com.ar";
 		$desde="From:"."SkySoft";
 		$asunto="[Comentario SkySoft] ".$_POST['asunto'];
-		$mensaje=" Nombre: ".$_POST['nombre']."\nEmail: ".$_POST['email']."\nMensaje: ".$_POST['comentario'];
-		mail($destino,$asunto,$mensaje,$mailLucas);
-		header('Location: enviado.html'); 
+		$mensaje="Nombre: ".$_POST['nombre']."\nEmail: ".$_POST['email']."\nMensaje: ".$_POST['comentario'];
+		mail($mailLucas,$asunto,$mensaje,$desde);
+		mail($mailWalter,$asunto,$mensaje,$desde);
+		header('Location: enviado.html');
 
 	}
 else
